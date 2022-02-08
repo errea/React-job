36179eff3efd7427cb16ced3a10145db6205444f
errea <erreakezy@gmail.com>
2022-02-08 15:30:44 +0100

    Add state object to store data in state

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import Table from './Table'
 
 class App extends Component {
+    state = {
+      characters: [
+        {
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+
+      ],
+    }
+
+  
     render() {
-
-        const characters = [
-            {
-              name: 'Charlie',
-              job: 'Janitor',
-            },
-            {
-              name: 'Mac',
-              job: 'Bouncer',
-            },
-            {
-              name: 'Dee',
-              job: 'Aspring actress',
-            },
-            {
-              name: 'Dennis',
-              job: 'Bartender',
-            },
-          ]
-
-        return (
-            <div className="container">
-                <Table characterData={characters} />
-            </div>
-        )
+      const { characters } = this.state
+    
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        </div>
+      )
     }
 }
 
